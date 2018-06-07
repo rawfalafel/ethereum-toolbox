0f755e63c372d6a3dda980ef1be682ee21c9c7d1
Yutaro Mori <ymori123@gmail.com>
2018-06-07 18:31:00 +0900

    Scaffolding for RLP marshaling

---
+++ rlp.go
@@ -0,0 +1,7 @@
+package main
+
+func Encode(val interface{}) byte[] {
+	// Before marshaling, determine the length of the final array
+	// Determine type with reflection library
+	// For structs and arrays, encode recursively
+}
