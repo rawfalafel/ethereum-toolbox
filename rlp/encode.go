d80fc804d5d115bed75cee3622390588082de675
Yutaro Mori <ymori123@gmail.com>
2018-06-13 20:57:17 +0900

    Pass tests for string and bigint

--- rlp/encode.go
+++ rlp/encode.go
@@ -30,7 +30,11 @@ type Item struct {
 	itemList []*Item
 }
 
+// TODO: Remove and use the actual interfacd
+type Encoder interface {}
+
 var (
+	encoderInterface = reflect.TypeOf(new(Encoder)).Elem()
 	bigInt = reflect.TypeOf(big.Int{})
 	big0   = big.NewInt(0)
 )
@@ -39,22 +43,31 @@ func getItem(v interface{}) (*Item, error) {
 	item := Item{ v: v }
 	err := errors.New("")
 
-	typeOf := reflect.TypeOf(v)
+	typ := reflect.TypeOf(v)
+	kind := typ.Kind()
 	switch {
-	case typeOf.Kind() == reflect.Array:
-		if item.size, item.itemList, err = getArray(v.([]interface{})); err != nil {
+	case typ.AssignableTo(reflect.PtrTo(bigInt)):
+		if item.size, err = getIntPtr(v.(*big.Int)); err != nil {
 			return nil, err
 		}
-	case typeOf.AssignableTo(bigInt):
+	case typ.AssignableTo(bigInt):
 		if item.size, err = getInt(v.(big.Int)); err != nil {
 			return nil, err
 		}
-	case isUint(typeOf.Kind()):
+	case isUint(typ.Kind()):
 		item.size = getUint(reflect.ValueOf(v).Uint())
-	case typeOf.Kind() == reflect.String:
+	case typ.Kind() == reflect.String:
 		item.size = getString(v.(string))
-	case typeOf.Kind() == reflect.Bool:
+	case typ.Kind() == reflect.Bool:
 		item.size = getBool()
+	case kind == reflect.Slice && isByte(typ.Elem()):
+		item.size = getString(v.(string))
+	case kind == reflect.Array && isByte(typ.Elem()):
+		item.size = getString(v.(string))
+	case kind == reflect.Array || kind == reflect.Slice:
+		if item.size, item.itemList, err = getArray(v.([]interface{})); err != nil {
+			return nil, err
+		}
 	default:
 		return nil, fmt.Errorf("rlp: unsupported item type")
 	}
@@ -106,14 +119,21 @@ func getArray(v []interface{}) (int, []*Item, error) {
 	return size, itemList, nil
 }
 
-func getInt(v big.Int) (int, error) {
-	if cmp := v.Cmp(big0); cmp == -1 {
+func getIntPtr(v *big.Int) (int, error) {
+	if v == nil {
+		return 1, nil
+	} else if cmp := v.Cmp(big0); cmp == -1 {
 		return 0, fmt.Errorf("rlp: cannot encode negative *big.Int")
 	} else if cmp == 0 {
 		return 1, nil
 	} else {
 		return getByteHeaderSize(v.Bytes()) + len(v.Bytes()), nil
 	}
+
+}
+
+func getInt(v big.Int) (int, error) {
+	return getIntPtr(&v)
 }
 
 func getString(v string) int {
@@ -125,19 +145,25 @@ func getBool() int {
 }
 
 func encodeItem(item *Item) []byte {
-	println("encodeItem")
 	data := make([]byte, 0, item.size)
 
-	typeOf := reflect.TypeOf(item.v)
+	typ := reflect.TypeOf(item.v)
+	kind := typ.Kind()
 	switch {
-	case typeOf.Kind() == reflect.Array:
-	case typeOf.Kind() == reflect.String:
+	case kind == reflect.Slice && isByte(typ.Elem()):
 		data = encodeString(data, item.v.(string))
-	case isUint(typeOf.Kind()):
+	case kind == reflect.Array && isByte(typ.Elem()):
+		data = encodeString(data, item.v.(string))
+	case typ.Kind() == reflect.Array:
+	case typ.Kind() == reflect.String:
+		data = encodeString(data, item.v.(string))
+	case isUint(typ.Kind()):
 		data = encodeUint(data, reflect.ValueOf(item.v).Uint())
-	case typeOf.AssignableTo(bigInt):
+	case typ.AssignableTo(reflect.PtrTo(bigInt)):
+		data = encodeIntPtr(data, item.v.(*big.Int))
+	case typ.AssignableTo(bigInt):
 		data = encodeInt(data, item.v.(big.Int))
-	case typeOf.Kind() == reflect.Bool:
+	case typ.Kind() == reflect.Bool:
 		data = encodeBool(data, item.v.(bool))
 	}
 
@@ -163,19 +189,20 @@ func encodeUint(data []byte, v uint64) []byte {
 	}
 }
 
-func encodeInt(data []byte, v big.Int) []byte {
+func encodeIntPtr(data []byte, v *big.Int) []byte {
 	if cmp := v.Cmp(big0); cmp == -1 {
 		panic("rlp: can not encode negative big.Int")
 	} else if cmp == 0 {
 		return append(data, 0x80)
+	} else if vb := v.Bytes(); len(vb) == 1{
+		return encodeByte(data, vb[0])
 	} else {
-		vb := v.Bytes()
-		if len(vb) == 1 {
-			return encodeByte(data, vb[0])
-		} else {
-			return encodeBytes(data, vb)
-		}
+		return encodeBytes(data, vb)
 	}
+}
+
+func encodeInt(data []byte, v big.Int) []byte {
+	return encodeIntPtr(data, &v)
 }
 
 func encodeBool(data []byte, v bool) []byte {
@@ -200,7 +227,6 @@ func encodeBytes(data []byte, b []byte) []byte {
 }
 
 func encodeByteHeader(data []byte, size int) []byte {
-	println("encodeByteHeader size", size)
 	if size < 56 {
 		return append(data, 0x80+byte(size))
 	} else {
@@ -212,7 +238,6 @@ func encodeByteHeader(data []byte, size int) []byte {
 }
 
 func convertBigEndian(num uint) []byte {
-	println("convertBigEndian")
 	data := make([]byte, 0)
 
 	for ; num >= 1; num = num >> 8 {
@@ -224,4 +249,8 @@ func convertBigEndian(num uint) []byte {
 
 func isUint(k reflect.Kind) bool {
 	return k >= reflect.Uint && k <= reflect.Uintptr
+}
+
+func isByte(typ reflect.Type) bool {
+	return typ.Kind() == reflect.Uint8 && !typ.Implements(encoderInterface)
 }
